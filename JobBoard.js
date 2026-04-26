1bc5347418ff6a2a62bf471701cb997c3f1e2589
RaineyMr <hire.mr.rainey@gmail.com>
2026-04-26 18:58:45 -0500

    Add files via upload

---
+++ JobBoard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function JobBoard() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold text-gray-900">JobBoard</h1>
+      <p className="text-gray-600 mt-2">Coming soon...</p>
+    </div>
+  );
+}
